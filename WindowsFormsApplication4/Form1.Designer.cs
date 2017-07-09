17bb1f8047fac358fbc8c7b81ef4b37b32af1750
Diaglyonok <Diaglyonok@yandex.ru>
2017-07-09 15:53:56 +0300

    added all files

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvl_up_down = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lvl_up_down)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(315, 298);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(342, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(599, 385);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 356);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Уровень";
+            // 
+            // lvl_up_down
+            // 
+            this.lvl_up_down.Location = new System.Drawing.Point(186, 377);
+            this.lvl_up_down.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.lvl_up_down.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.lvl_up_down.Name = "lvl_up_down";
+            this.lvl_up_down.Size = new System.Drawing.Size(120, 20);
+            this.lvl_up_down.TabIndex = 6;
+            this.lvl_up_down.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(953, 431);
+            this.Controls.Add(this.lvl_up_down);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lvl_up_down)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown lvl_up_down;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        List<Color> colors;
+        Color mainColor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Draw_Levy(GraphicsPath path, float x1, float x2, float y1, float y2, int i)
+        {
+            float x3, y3;
+
+            if (i == 0)
+                path.AddLine(x1, y1, x2, y2);
+            else
+            {
+                x3 = (x1 + x2) / 2 + (y2 - y1) / 2;
+                y3 = (y1 + y2) / 2 - (x2 - x1) / 2;
+
+                Draw_Levy(path, x1, x3, y1, y3, i - 1);
+                Draw_Levy(path, x3, x2, y3, y2, i - 1);
+            }
+        }
+
+        GraphicsPath path = new GraphicsPath();
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            pictureBox1.CreateGraphics().DrawPath(new Pen(mainColor), path);
+        }
+
+        void Build(int iterations)
+        {
+            path = new GraphicsPath();
+            Draw_Levy(path, 80, 230, 150, 150, iterations);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lvl_up_down.Text = "1";
+            mainColor = Color.Black;
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+            BackColor = Color.White;
+            pictureBox2.Image = Image.FromFile("../../res/tree.png");
+            pictureBox2.SizeMode = pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            initColors();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.CreateGraphics().Clear(Color.White);
+
+            int depth = Convert.ToInt32(lvl_up_down.Value);
+            if (depth < 1)
+            {
+                depth = 1;
+                lvl_up_down.Value = 1;
+            }
+            Build(Convert.ToInt32(depth));
+
+            mainColor = colors.ElementAt<Color>(depth - 1);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Point p = pictureBox1.PointToClient(MousePosition);
+            lvl_up_down.Text = getDepth(p.Y) + "";
+
+        }
+
+
+        public int getDepth(int Y)
+        {
+            if (Y > 0 && Y <= 23) return 1;
+            if (Y > 23 && Y <= 46) return 2;
+            if (Y > 46 && Y <= 76) return 3;
+            if (Y > 76 && Y <= 104)  return 4;
+            if (Y > 104 && Y <= 130) return 5;
+            if (Y > 130 && Y <= 155) return 6;
+            if (Y > 155 && Y <= 182) return 7;
+            if (Y > 182 && Y <= 208) return 8;
+            if (Y > 208 && Y <= 234) return 9;
+            if (Y > 234 && Y <= 264) return 10;
+            if (Y > 264 && Y <= 289) return 11;
+            if (Y > 289 && Y <= 317) return 12;
+            if (Y > 317 && Y <= 341) return 13;
+            if (Y > 341 && Y <= 369) return 14;
+            if (Y > 369 && Y <= 384) return 15;
+
+            return 0;
+        }
+
+        public void initColors()
+        {
+            colors = new List<Color>();
+            colors.Add(Color.FromArgb(91, 155, 213)); //1 (Голубой)
+            colors.Add(Color.FromArgb(255, 0, 0)); //2 (Красный)
+            colors.Add(Color.FromArgb(146, 208, 80)); //3 (Зеленый)
+            colors.Add(Color.FromArgb(255,255,0)); //4 (Желтый)
+            colors.Add(Color.FromArgb(237, 125, 49)); //5 (Оранжевый)
+            colors.Add(Color.FromArgb(112, 48, 160)); //6 (Фиолет)
+            colors.Add(Color.FromArgb(192, 0, 0)); //7 (Темно красный)
+            colors.Add(Color.FromArgb(0, 254, 254)); //8 (Бирюзовый)
+            colors.Add(Color.FromArgb(0, 0, 0)); //9 (Черный)
+            colors.Add(Color.FromArgb(127, 127, 127)); //10 (Серый)
+            colors.Add(Color.FromArgb(0, 32, 96)); //11 (Синий)
+            colors.Add(Color.FromArgb(163, 191, 0)); //12 (Салатовый)
+            colors.Add(Color.FromArgb(255, 40, 148)); //13 (Розовый)
+            colors.Add(Color.FromArgb(255, 192, 0)); //14 (Желто-Оранжевый)
+            colors.Add(Color.FromArgb(0, 112, 192)); //15 (Темно голубой)
+        }
+
+    }
+}
